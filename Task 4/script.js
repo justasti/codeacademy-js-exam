16074362c0cd65cb0ed19154b5f431f625482483
Justas Tirevicius <justas.tirevicius@gmail.com>
2022-03-05 13:14:49 +0200

    feat: add logic to fetch brands and models and put them into corresponing html elements

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,4 +9,17 @@ Pastaba: Informacija apie automobilį (brand) (jo kortelė) bei turi turėti
 bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = "cars.json";
+const output = document.getElementById("output");
+fetch(ENDPOINT)
+  .then((response) => response.json())
+  .then((data) => {
+    for (let i = 0; i < data.length; i++) {
+      output.innerHTML += `<div class="brand"><h2>${data[i].brand}</h2><ul class="models"></ul></div>`;
+      for (let j = 0; j < data[i].models.length; j++) {
+        document.querySelectorAll(".models")[
+          i
+        ].innerHTML += `<li>${data[i].models[j]}</li>`;
+      }
+    }
+  });
