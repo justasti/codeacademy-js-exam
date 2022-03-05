978e848a3f9a9d0052653106c8880ba1e5dbe50f
Justas Tirevicius <justas.tirevicius@gmail.com>
2022-03-05 14:20:11 +0200

    feat: complete task - created constructor function having 4 methods - sum, substraction, multiplication, division

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,18 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+function Calculator() {
+  this.sum = function (a, b) {
+    return a + b;
+  };
+  this.subtraction = function (a, b) {
+    return a - b;
+  };
+  this.multiplication = function (a, b) {
+    return a * b;
+  };
+  this.division = function (a, b) {
+    return a / b;
+  };
+}
+const calc = new Calculator();
