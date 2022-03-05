b496d6bec576d22d71314d2e2650e8f040fa7115
Justas Tirevicius <justas.tirevicius@gmail.com>
2022-03-05 14:01:19 +0200

    feat: add logic for showObjectKeys to return object keys as an array

--- Task 7/script.js
+++ Task 7/script.js
@@ -5,8 +5,11 @@ Parašykite funkciją showObjectKeys, kuri kaip argumentą priims objectą
 ir grąžins visus jo "key" masyve.
 -------------------------------------------------------------------------- */
 const audi = {
-  make: 'audi',
-  model: 'A6',
+  make: "audi",
+  model: "A6",
   year: 2005,
-  color: 'white',
+  color: "white",
+};
+showObjectKeys = (obj) => {
+  return Object.keys(obj);
 };
