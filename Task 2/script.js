656fb41b7d1de27c033d1c1fd3438a744957619f
Justas Tirevicius <justas.tirevicius@gmail.com>
2022-03-05 12:12:28 +0200

    feat: add logic to count button clicks

--- Task 2/script.js
+++ Task 2/script.js
@@ -3,3 +3,8 @@ Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
 pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 ------------------------------------------------------------------- */
+let count = 0;
+document.getElementById("btn__element").addEventListener("click", () => {
+  count++;
+  document.getElementById("btn__state").textContent = count;
+});
