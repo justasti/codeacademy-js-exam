0bc75c99d22679f58e222864298ff798b1ab9fdf
Justas Tirevicius <justas.tirevicius@gmail.com>
2022-03-05 14:28:13 +0200

    feat: create class Movie with 3 properties and add a method called wasExpensive

--- Task 9/script.js
+++ Task 9/script.js
@@ -6,3 +6,18 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnę nei 100 000 000 mln USD, tada gražins true, kiru atveju false 
 ------------------------------------------------------------------------------------------------------ */
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+  wasExpensive = () => {
+    return this.budget > 100000000;
+  };
+}
+const silenceOfTheLambs = new Movie(
+  "The Silence of the Lambs",
+  "J. Demme",
+  19000000
+);
