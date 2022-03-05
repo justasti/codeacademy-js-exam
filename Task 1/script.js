6cff121754e0b6701e605f70883346f2c21d7ea3
Justas Tirevicius <justas.tirevicius@gmail.com>
2022-03-05 11:54:42 +0200

    feat: add logic for weight converter to convert units

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,22 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+const form = document.querySelector("form");
+const input = document.getElementById("search");
+const output = document.getElementById("output");
+const regex = /^[0-9]+$/;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (input.value !== "" && input.value.match(regex)) {
+    output.innerHTML = `1. ${input.value}kg is ${
+      input.value * 2.2046
+    }lb<br>2. ${input.value}kg is ${input.value / 0.001}g<br>3. ${
+      input.value
+    }kg is ${input.value * 35.274}oz<br>`;
+    input.value = "";
+  } else {
+    alert("please enter a number!");
+    input.value = "";
+  }
+});
